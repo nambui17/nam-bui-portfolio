7bb5fa87aba26e0d9bd2d90280684d09891ac926
Nam Bui <nam.bui17@icloud.com>
2025-04-15 21:33:59 -0500

    Outline homepage with timeline
    Outline homepage with timeline of jobs and job descriptions. Add a link to download Resume.

---
+++ client/src/components/Navbar/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { HStack } from "@chakra-ui/react";
+
+import pdf from '../../assets/Resume-2025.pdf'
+import './style.css'
+
+export default function Navbar() {
+    return (
+        <HStack 
+            display={"flex"}
+            justifyContent={"end"}
+            id="navbar">
+            <Link to="/" className="link">Home</Link>
+            <a className="link" target="_blank" href={pdf}>CV</a>
+            <Link to="/projects" className="link">Projects</Link>
+            <Link to="/contact" className="link">Contact Me</Link>
+        </HStack>
+    )
+}
